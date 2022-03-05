5b79f39e5b96df3fbac56992ad98a5bc73417d3f
Åsa Sieurin <asa.sieurin@gmail.com>
2022-03-05 20:33:11 +0100

    small fixes

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// timeout-function for spinner
-setTimeout(function () {
-  const loader = document.querySelector(".loader");
-  const content = document.querySelector(".main");
-  loader.style.animation = "shrink 2s forwards";
-  content.style.display = "block";
-}, 4000);
+
