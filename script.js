407434f3e677a99f8e5ddae8ee68682f8d5d8f09
Åsa Sieurin <asa.sieurin@gmail.com>
2022-08-16 16:31:30 +0200

    new accordion, new projects, small adjustments regarding responsivenes

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function setRedMode() {
 function setLightMode() {
     document.body.classList.remove('red');
 }
+
+// A function that adds and remove the class "active" on the section you click on.
+function toggle() {
+  this.classList.toggle("active")
+}
+
+// Selects a "section", and calls a function which will be executed when the element is clicked.
+document.getElementById("section1").onclick = toggle
+document.getElementById("section2").onclick = toggle
+document.getElementById("section3").onclick = toggle
+document.getElementById("section4").onclick = toggle
+document.getElementById("section5").onclick = toggle
+document.getElementById("section6").onclick = toggle
+document.getElementById("section7").onclick = toggle
+document.getElementById("section8").onclick = toggle
+document.getElementById("section9").onclick = toggle
+document.getElementById("section10").onclick = toggle
+
